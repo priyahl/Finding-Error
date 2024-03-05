025c118798f128bbb5e12a828bd1b139d1ac8b74
priyahl <hlpriya125@gmail.com>
2024-03-05 18:19:54 +0530

    commit

---
+++ src/main/java/com/findingError/FindingErrorApplication.java
@@ -0,0 +1,13 @@
+package com.findingError;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindingErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindingErrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findingError/FindingErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findingError;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindingErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
